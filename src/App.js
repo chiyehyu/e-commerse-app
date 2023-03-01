2dce6a46a94265ceba65e6465b0e6d9552abe608
Adam Yu <chiyehyu@gmail.com>
2023-03-01 22:25:21 +0800

    react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Checkout,
+  PrivateRoute,
+  Error,
+  AuthWrapper,
+  SharedLayout,
+} from './pages';
+
+import { useUserContext } from './context/user_context';
+
+const Container = styled.div`
+  background: green;
+  color: white;
+  .hero {
+    font-size: 8rem;
+  }
+  h2 {
+    color: red;
+  }
+`;
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  const { user, setUser } = useUserContext();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* parent page to put shared layout: navbar, footer, ... */}
+        <Route path="/" element={<SharedLayout />}>
+          {/* index will match the path to the parent */}
+          <Route index element={<Home />} /> {/* public pages */}
+          <Route path="about" element={<About />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:productid" element={<SingleProduct />} />
+          {/* authorized pages */}
+          <Route
+            path="checkout"
+            element={
+              <PrivateRoute user={user}>
+                <Checkout user={user} />
+              </PrivateRoute>
+            }
+          />
+          {/* login page */}
+          {/* <Route path="login" element={<Login setUser={setUser} />} /> */}
+          {/* 404 page */}
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+
+      {/* <Container>
+        Inside Container
+        <div className="hero">with className=hero</div>
+      </Container> */}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+//using index for group import / export
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+import PrivateRoute from './PrivateRoute';
+import Error from './ErrorPage';
+import AuthWrapper from './AuthWrapper';
+import SharedLayout from './SharedLayout';
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Checkout,
+  PrivateRoute,
+  Error,
+  AuthWrapper,
+  SharedLayout,
+};
