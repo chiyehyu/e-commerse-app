b355a846dfad4ff95fca7c728242c30325d0c9ac
Adam Yu <chiyehyu@gmail.com>
2023-03-14 12:36:05 +0800

    Cart finished

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -40,6 +40,43 @@ const cart_reducer = (state, action) => {
       return { ...state, cart: [...state.cart, newItem] };
     }
   }
+  if (action.type === REMOVE_CART_ITEM) {
+    const tmpCart = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: tmpCart };
+  }
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, value } = action.payload;
+    const tmpCart = state.cart.map((item) => {
+      if (item.id === id) {
+        let newAmount = item.amount + value;
+        if (newAmount > item.stock) {
+          newAmount = item.stock;
+        }
+        if (newAmount < 1) {
+          newAmount = 1;
+        }
+        return { ...item, amount: newAmount };
+      } else {
+        return item;
+      }
+    });
+    return { ...state, cart: tmpCart };
+  }
+  if (action.type === COUNT_CART_TOTALS) {
+    const { totalItems, totalAmount } = state.cart.reduce(
+      (total, item) => {
+        const { amount, price } = item;
+        total.totalItems += amount;
+        total.totalAmount += amount * price;
+        return total;
+      },
+      { totalItems: 0, totalAmount: 0 }
+    );
+    return { ...state, totalItems, totalAmount };
+  }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
